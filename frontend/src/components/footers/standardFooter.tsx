d7eb30b714646a7849dd412d235c6dfa3b7edeb9
whyweexist <167321903+whyweexist@users.noreply.github.com>
2024-08-02 04:43:44 +0530

    Update standardFooter.tsx

--- frontend/src/components/footers/standardFooter.tsx
+++ frontend/src/components/footers/standardFooter.tsx
@@ -2,7 +2,7 @@ export default function StandardFooter() {
   return (
     <footer className="footer footer-center text-xs p-4 border-t border-base-300">
       <aside>
-        <p>Copyright © {new Date().getFullYear()} - All right reserved by FundsNav Ltd</p>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved Ltd</p>
       </aside>
     </footer>
   );
